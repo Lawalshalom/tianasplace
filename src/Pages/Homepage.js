cc01e4a6554255b481af0e254662216e9a1d4342
Femi Lawal <lawalshalom@gmail.com>
2020-07-30 02:51:23 +0100

    favicon

--- src/Pages/Homepage.js
+++ src/Pages/Homepage.js
@@ -58,7 +58,7 @@ export default class Homepage extends Component {
             <main className="home-main">
                 <div className="container">
                     <div className="header-logo">
-                        <img src="https://res.cloudinary.com/lawfirm1000/image/upload/v1595544226/Portfolio%20site/f3f7eb57-efb5-4062-92e8-c6e001a43af7_toamzn.png" alt="header logo"/>
+                        <img src="https://res.cloudinary.com/lawfirm1000/image/upload/v1596073559/Portfolio%20site/imageedit_1_4636924411_w5k4ii.png" alt="header logo"/>
                     </div>
                     <div className="home">
                         <div className="home-text line1">
